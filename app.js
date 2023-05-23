29dc4aa82ecb60af379e8d7a9fdcd47fd2420dd2
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2023-05-23 22:51:42 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/////////////////////////////////////////////////////////////////
+////////////////////////Const and vars///////////////////////////
 const muted = document.getElementById("muted")
 const low_fart = document.getElementById("low_fart");
 const medium_fart = document.getElementById("medium_fart");
@@ -9,8 +11,19 @@ let hz_range_value = document.getElementById("hz_range_value");
 let low_fart_audio = new Audio("sound/low_fart.mp3");
 let medium_fart_audio = new Audio("sound/medium_fart.mp3");
 let high_fart_audio = new Audio("sound/high_fart.mp3");
+let audio_array = [low_fart_audio, medium_fart_audio, high_fart_audio];
+/////////////////////////////////////////////////////////////////
 
 hz_range.oninput = function(){
+
+    //Reset audio to pause on hz_range change :
+    for(let i = 0; i < audio_array.length; i++){
+        audio_array[i].pause();
+        audio_array[ik].currentTime = 0;  
+    }
+
+    
+    
 
     //Fart choice detector :
     console.log("Etat low:" + low_fart.checked);
@@ -19,6 +32,7 @@ hz_range.oninput = function(){
 
     //Si muted je return :
     if(muted.checked === true){
+        onValueChange();
         return;
     }
     
