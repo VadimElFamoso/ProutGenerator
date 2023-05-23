6fafced76d3b056be113aa4fd5079df82054abc3
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2023-05-23 22:44:28 +0200

    Je me faisais chier

---
+++ app.js
@@ -0,0 +1,52 @@
+const muted = document.getElementById("muted")
+const low_fart = document.getElementById("low_fart");
+const medium_fart = document.getElementById("medium_fart");
+const high_fart = document.getElementById("high_fart");
+
+const hz_range = document.getElementById("hz_range");
+let hz_range_value = document.getElementById("hz_range_value");
+
+let low_fart_audio = new Audio("sound/low_fart.mp3");
+let medium_fart_audio = new Audio("sound/medium_fart.mp3");
+let high_fart_audio = new Audio("sound/high_fart.mp3");
+
+hz_range.oninput = function(){
+
+    //Fart choice detector :
+    console.log("Etat low:" + low_fart.checked);
+    console.log("Etat medium:" + medium_fart.checked);
+    console.log("Etat high:" + high_fart.checked);
+
+    //Si muted je return :
+    if(muted.checked === true){
+        return;
+    }
+    
+    //On vérifie une par une chacune de ces conditions : 
+    else if(low_fart.checked === true){
+        playAudio(low_fart_audio, onValueChange());
+    }
+
+    else if(medium_fart.checked === true){
+        playAudio(medium_fart_audio, onValueChange());
+    }
+    else if(high_fart.checked === true){
+        playAudio(high_fart_audio, onValueChange());
+    }
+    else{
+        return;
+    }
+};
+
+//A chaque changement de valeur, je la recalcule : 
+function onValueChange(){
+    let value = hz_range.value;
+    hz_range_value.innerHTML = value + " Hz";
+    return value;
+}
+
+//Fonction qui joue l'audio avec la valeur en Hz définie par l'utilisateur : 
+function playAudio(audio, hz_value){
+        audio.play();
+        audio.playbackRate = hz_value;
+}
